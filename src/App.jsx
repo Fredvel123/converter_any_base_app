e70c722a718558c33ffa163fc638b11f0b30d6f7
freddy velarde <freddyvelarde59#gmail.com>
2022-08-08 18:22:13 -0400

    theme stored in localstorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { setTheme } from './redux/slices/theme';
+
 function App() {
-  return (
-    <div className="App">
-      <h2>hello world</h2>
-    </div>
-  );
+	const theme = useSelector((state) => state.theme.value);
+	const dispatch = useDispatch();
+	useEffect(() => {
+		const data = localStorage.getItem('theme');
+		if (data !== null) {
+			dispatch(setTheme(JSON.parse(data)));
+		}
+		// eslint-disable-next-line
+	}, []);
+
+	useEffect(() => {
+		localStorage.setItem('theme', JSON.stringify(theme));
+	}, [theme]);
+
+	return (
+		<div className="App">
+			<h2>hello world</h2>
+		</div>
+	);
 }
 
 export default App;
