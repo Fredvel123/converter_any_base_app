bcff4cdfb9d81c930d52ede667aad08323750e88
freddy velarde <freddyvelarde59#gmail.com>
2022-08-09 11:42:20 -0400

    install .sh added

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ https://base-converter-umsa.netlify.app/
 
 # Instalation
 
-If you have bash already installed, you can execute the next comand
+If you wanna install the whole on your own machine, please execute the next comand
 
 ```bash
 curl "https://raw.githubusercontent.com/Fredvel123/converter_any_base_app/master/instalation.sh" | bash
--- instalation.sh
+++ instalation.sh
@@ -9,8 +9,8 @@ git clone https://github.com/Fredvel123/converter_any_base_app
 
 # installing project with yarn
 echo "What's your favorite package manager? just type yarn or npm"
-
 read npm_package 
+
 if [ $npm_package == 'yarn' ] || [ $npm_package == 'npm' ]
 then 
 echo " ----- Installing packages with $npm_package ---- "
@@ -18,12 +18,13 @@ echo " ----- Installing packages with $npm_package ---- "
 sleep 3
 
 echo "**** ALERT: Installing React Packages ****"
-cd ~/converter_any_base_app
+cd converter_any_base_app
 
 $npm_package install
 echo "----- React installed"
 sleep 2 
 echo "----- ALERT: Fullstack project installed"
+
 else 
 echo 'WARNING: Only use npm or yarn package manager'
 fi
